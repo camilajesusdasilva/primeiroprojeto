e34df2f70e7c556e6711b1c4da807c18a7088a5a
Camila <silvacamilajesusda@gmail.com>
2024-11-09 14:39:23 -0300

    new file:   .gitignore new file:   .idea/.gitignore new file:   .idea/misc.xml new file:   .idea/modules.xml new file:   .idea/uiDesigner.xml new file:   .idea/vcs.xml new file:   primeiroprojeto.iml new file:   src/Atividades/Estudos.java new file:   src/Atividades/Estudos/anotacoes new file:   src/Atividades/Perfil.java modified:   src/Atividades/Estudos.java modified:   src/Atividades/Perfil.java

---
+++ src/Atividades/Estudos.java
@@ -0,0 +1,14 @@
+package Atividades;
+
+import java.util.Scanner;
+
+public class Perfil{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite seu nome: ");
+    String nome = scanner.next();
+    System.out.println("Bem vindo, " + nome + "! Aproveite nossa aplicação.");
+    }
+}
+
