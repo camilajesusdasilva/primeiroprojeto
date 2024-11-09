c0beb5c405134b5d1ed4ba3cf518989f42b10bdb
Camila <silvacamilajesusda@gmail.com>
2024-11-09 14:39:59 -0300

    modified:   src/Atividades/Estudos.java modified:   src/Atividades/Perfil.java

--- src/Atividades/Estudos.java
+++ src/Atividades/Estudos.java
@@ -1,14 +1,38 @@
 package Atividades;
 
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-public class Perfil{
+public class Estudos {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        String filePath = "atividades/estudos/anotacoes.txt";
 
-    System.out.println("Digite seu nome: ");
-    String nome = scanner.next();
-    System.out.println("Bem vindo, " + nome + "! Aproveite nossa aplicação.");
+        System.out.println("Escolha uma opção: ");
+        System.out.println("1 - Fazer uma anotação");
+        System.out.println("2 - Ver anotações já feitas");
+
+        int opcao = scanner.nextInt();
+        scanner.nextLine();
+
+        if (opcao == 1){
+            System.out.println("Faça suas anotações: ");
+            String anotacao = scanner.nextLine();
+
+            try (FileWriter writer = new FileWriter(filePath, true)){
+                writer.write(anotacao + "n");
+                System.out.println("Anotação salva!");
+            } catch (IOException e) {
+                System.out.println("Erro ao salvar a anotação: " + e.getMessage());
+            }
+        } else if (opcao == 2) {
+            System.out.println("Anotações salvas: ");
+
+        }
     }
 }
 
